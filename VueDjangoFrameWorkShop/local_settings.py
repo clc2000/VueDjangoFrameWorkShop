ea8aa53984dfb10f1eef7fac2354e97e8804b1e6
clc2000 <cuichao2002_0725@sina.com>
2019-11-14 02:27:31 +0000

    20191114 local modification for me

--- VueDjangoFrameWorkShop/local_settings.py
+++ VueDjangoFrameWorkShop/local_settings.py
@@ -10,7 +10,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vue_shop',
         'USER': 'root',
-        'PASSWORD': 'mtianyanroot',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         "OPTIONS": {"init_command": "SET default_storage_engine=INNODB;"}
     }
---
+++ vue3x/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/calix/workd/VueDjangoFrameWorkShop/vue3x/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
